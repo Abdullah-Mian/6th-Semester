2d19603d8cfc88c82c10adfd719dc2e0baf23106
Abdullah Mian <aabdullahmian@gmail.com>
2025-04-05 22:51:01 +0500

    ..

--- AI LAB/simple_linear_regression.py
+++ AI LAB/simple_linear_regression.py
@@ -17,16 +17,25 @@ import pandas as pd
 
 """## Importing the dataset"""
 
-dataset = pd.read_excel('/content/Salary_Data.xlsx')
+dataset = pd.read_excel("/content/Salary_Data.xlsx")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+print("valeus of X")
+print(X)
+print("valeus of y")
+print(y)
 
 """## Splitting the dataset into the Training set and Test set"""
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+# import sklearn.model_selection
+# X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 1/3, random_state =0)
 
-"""## Training the Simple Linear Regression model on the Training set"""
+print((X_train))
+
+""" Training the Simple Linear Regression model on the Training set"""
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
@@ -39,7 +48,7 @@ y_pred = regressor.predict(X_test)
 """## Visualising the Training set results"""
 
 plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.plot(X_train, regressor.predict(X_train), color = 'green')
 plt.title('Salary vs Experience (Training set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
@@ -56,7 +65,9 @@ plt.ylabel('Salary')
 plt.grid()
 plt.show()
 
-regressor.predict([[5]])
+regressor.predict([[6]])
+
+
 
 print(regressor.coef_)
 
